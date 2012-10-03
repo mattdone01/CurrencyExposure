6f247bc784d02be60fe0ca182e78df641c78f852
Matt Done <mattdone@gmail.com>
2012-10-03 23:31:11 +1000

    Uploaded to web

--- CurrencyExposure.Repository/CurrencyExposureContext.cs
+++ CurrencyExposure.Repository/CurrencyExposureContext.cs
@@ -10,7 +10,7 @@ namespace CurrencyExposure.Repository
 		public DbSet<BlogCategory> BlogCategorys { get; set; }
 		public DbSet<BlogComment> BlogComments { get; set; }
 		public DbSet<BlogSocialLink> BlogSocialLinks { get; set; }
-		public DbSet<UserProfile> UserProfiles { get; set; }
+		//public DbSet<UserProfile> UserProfiles { get; set; }
 		public DbSet<ContactUs> ContactUs { get; set; }
 		public bool IsDisposed { get; set; }
 
